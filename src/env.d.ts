496cb7c29db92176ce406ee3778b87c6c71d04e3
Babs90000 <b.camara.diaby@outlook.com>
2026-03-05 09:07:42 +0000

    feat: layout principal et styles globaux

---
+++ src/env.d.ts
@@ -0,0 +1,14 @@
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly SMTP_HOST: string;
+  readonly SMTP_PORT: string;
+  readonly SMTP_USER: string;
+  readonly SMTP_PASS: string;
+  readonly SMTP_TO: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
